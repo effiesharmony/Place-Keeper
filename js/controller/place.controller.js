bdfa1f3eb6add496f68341f768b55a3b7c0b31ff
Effie Armony <harmony.effie@gmail.com>
2024-09-18 20:57:41 +0300

    Create and render places-list & remove btn

---
+++ js/controller/place.controller.js
@@ -0,0 +1,19 @@
+'use strict'
+
+function onInit() {
+    setBgc()
+    renderPlaces()
+}
+
+function renderPlaces() {
+    const elList = document.querySelector('.places-list')
+    let strHTML = ''
+    gPlaces.forEach(place => strHTML += `<li>${place.name}
+        <button onclick="onRemovePlace('${place.id}')">X</button> </li>`)
+    elList.innerHTML = strHTML
+}
+
+function onRemovePlace(placeId) {
+    gPlaces = gPlaces.filter(place => place.id !== placeId)
+    renderPlaces()
+}
---
+++ js/services/placeService.js
@@ -0,0 +1,20 @@
+'use strict'
+// MAIN JS - CRUDL
+
+var gPlaces = [
+    {
+        id: 'p1',
+        lat: 32.1416,
+        lng: 34.831213,
+        name: `Puki's house`
+    },
+
+    {
+        id: 'p2',
+        lat: 32.076448,
+        lng: 34.818711,
+        name: `Luli's house`
+    }
+
+]
+
